731bb3a22e2fd87e3404d9f54f99512ba7688007
Miguel Saavedra <miguelernesto911@gmail.com>
2020-08-20 19:54:41 -0500

    fix

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "example-terraform-project-name-bootstrap-terraform-state"
+    bucket = "my-tf-test-bucket-miguel-tfstate"
     key    = "default-infrastructure.tfstate"
     region = "us-east-1"
   }
